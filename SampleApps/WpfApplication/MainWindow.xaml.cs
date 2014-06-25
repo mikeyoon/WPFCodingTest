c5a9e15692adb49a35d9ebcb4b7274b688db07d4
Johnathan Wortley <jwortley@airware.com>
2014-06-25 10:32:19 -0700

    * Cleanup the comments

--- SampleApps/WpfApplication/MainWindow.xaml.cs
+++ SampleApps/WpfApplication/MainWindow.xaml.cs
@@ -12,13 +12,6 @@ namespace WpfApplication
 {
     public partial class MainWindow : Window
     {
-
-        private SamplePoint movingPoint = new SamplePoint
-        {
-            Name = "Moving",
-            Location = new Location(53.5, 8.25)
-        };
-
         public MainWindow()
         {
             switch (Properties.Settings.Default.TileCache)
@@ -37,32 +30,17 @@ namespace WpfApplication
 
             // Static method of adding data. ViewModels are better :)
             var polylines = (ICollection<object>)Resources["Polylines"];
-            //polylines.Add(
-            //    new SamplePolyline
-            //    {
-            //        Locations = LocationCollection.Parse("53.5140,8.1451 53.5123,8.1506 53.5156,8.1623 53.5276,8.1757 53.5491,8.1852 53.5495,8.1877 53.5426,8.1993 53.5184,8.2219 53.5182,8.2386 53.5195,8.2387")
-            //    });
-            //polylines.Add(
-            //    new SamplePolyline
-            //    {
-            //        Locations = LocationCollection.Parse("53.5978,8.1212 53.6018,8.1494 53.5859,8.1554 53.5852,8.1531 53.5841,8.1539 53.5802,8.1392 53.5826,8.1309 53.5867,8.1317 53.5978,8.1212")
-            //    });
-              
-            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.1) };
-            timer.Tick += MovePoint;
-            timer.Start();
+            polylines.Add(
+                new SamplePolyline
+                {
+                    Locations = LocationCollection.Parse("53.5140,8.1451 53.5123,8.1506 53.5156,8.1623 53.5276,8.1757 53.5491,8.1852 53.5495,8.1877 53.5426,8.1993 53.5184,8.2219 53.5182,8.2386 53.5195,8.2387")
+                });
+            polylines.Add(
+                new SamplePolyline
+                {
+                    Locations = LocationCollection.Parse("53.5978,8.1212 53.6018,8.1494 53.5859,8.1554 53.5852,8.1531 53.5841,8.1539 53.5802,8.1392 53.5826,8.1309 53.5867,8.1317 53.5978,8.1212")
+                });
           
-        }
-
-        private void MovePoint(object sender, EventArgs e)
-        {
-            movingPoint.Location = new Location(movingPoint.Location.Latitude + 0.001, movingPoint.Location.Longitude + 0.002);
-
-            if (movingPoint.Location.Latitude > 54d)
-            {
-                movingPoint.Name = "Stopped";
-                ((DispatcherTimer)sender).Stop();
-            }
         }
 
         private void MapMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
